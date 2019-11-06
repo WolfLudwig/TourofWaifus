16fb4c00c8286b633b1067fd1bee566c9eb448d4
WolfLudwig <wolf_ludwig@outlook.com>
2019-11-06 15:12:23 +0100

    Displaying a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Selene' },
+  { id: 12, name: 'Eos' },
+  { id: 13, name: 'Yda' },
+  { id: 14, name: 'Alisaie' },
+  { id: 15, name: 'Yotsuyu' },
+  { id: 16, name: 'Ahmasis' },
+  { id: 17, name: 'Anna' },
+  { id: 18, name: 'Dorothea' },
+  { id: 19, name: 'Lysithea' },
+  { id: 20, name: 'Sonya' }
+];
